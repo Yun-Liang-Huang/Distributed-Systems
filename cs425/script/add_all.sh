1f9b099543464bb1a33e908eaacc989a525bf853
Yun-Liang Huang <35072527+Yun-Liang-Huang@users.noreply.github.com>
2022-12-13 14:41:06 -0600

    Delete cs425 directory

--- cs425/script/add_all.sh
+++
@@ -1,8 +0,0 @@
-INTRODUCER=fa22-cs425-0402.cs.illinois.edu
-PORT=8088
-
-for CLUSTER_NUMBER in $(seq -w 1 10)
-do
-    ./client membership join fa22-cs425-04${CLUSTER_NUMBER}.cs.illinois.edu $PORT $INTRODUCER $PORT
-    # sleep 1
-done
--- cs425/script/store_all.sh
+++
@@ -1,8 +0,0 @@
-PORT=8088
-
-for CLUSTER_NUMBER in $(seq -w 1 10)
-do
-    echo "================= ${CLUSTER_NUMBER} ====================="
-    ./client sdfs store fa22-cs425-04${CLUSTER_NUMBER}.cs.illinois.edu $PORT
-    # sleep 1
-done
